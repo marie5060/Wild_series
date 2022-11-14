1acc2a4f95ec95f9229b9a8913bf127841240524
marie5060 <marie5060@hotmail.com>
2022-11-14 07:47:38 +0100

    CategoryController created, view for all categories and programs in one category

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -5,24 +5,34 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProgramRepository;
 
 #[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
     
-    #[Route('/', name: 'program_index')]
-    public function index(): Response
+    #[Route('/', name: 'index')]
+    public function index(ProgramRepository $programRepository): Response
     {
+        $programs = $programRepository->findAll();
         return $this->render('program/index.html.twig', [
-            'website' => 'Wild Series',
+             'programs' => $programs
          ]);
     }
 
-    #[Route('/{id}', methods: ['GET'], requirements: ['id'=>'\d+'], name: 'program_serie')]
-    public function show(): Response
+    #[Route('/{id<^[0-9]+$>}', name: 'show')]
+    public function show(int $id, ProgramRepository $programRepository):Response
     {
+        $program = $programRepository->findOneBy(['id' => $id]);
+        // same as $program = $programRepository->find($id);
+    
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$id.' found in program\'s table.'
+            );
+        }
         return $this->render('program/show.html.twig', [
-            'id' => '3',
+            'program' => $program,
         ]);
     }
 }
