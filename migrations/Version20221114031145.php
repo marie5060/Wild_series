3d480fa5581341a5a86acadd615d1e78216ab2ad
marie5060 <marie5060@hotmail.com>
2022-11-14 04:17:24 +0100

    relation ManyToOne created

---
+++ migrations/Version20221114031145.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221114031145 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE category CHANGE id id INT AUTO_INCREMENT NOT NULL');
+        $this->addSql('ALTER TABLE program ADD category_id INT NOT NULL, CHANGE synopsis synopsis LONGTEXT NOT NULL');
+        $this->addSql('ALTER TABLE program ADD CONSTRAINT FK_92ED778412469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('CREATE INDEX IDX_92ED778412469DE2 ON program (category_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE category CHANGE id id INT AUTO_INCREMENT NOT NULL COMMENT \'Primary Key\'');
+        $this->addSql('ALTER TABLE program DROP FOREIGN KEY FK_92ED778412469DE2');
+        $this->addSql('DROP INDEX IDX_92ED778412469DE2 ON program');
+        $this->addSql('ALTER TABLE program DROP category_id, CHANGE synopsis synopsis TEXT NOT NULL');
+    }
+}
---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+//référence à l'entité que tu vas manipuler
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+
+    const CATEGORIES = [
+        "Action",
+        "Aventure",
+        "Animation",
+        "Fantastique"
+    ];
+    //la méthode load qui est appelée lors du chargement des fixtures
+    public function load(ObjectManager $manager)
+    {
+        foreach(self::CATEGORIES as $key => $categoryName){
+        //instanciation d'un nouvel objet Category
+        $category = new Category();
+        //la définition du nom de cette nouvelle catégorie
+        $category->setName( $categoryName);
+        //la persistance en base de données
+        $manager->persist($category);
+    }
+        $manager->flush();
+    }
+}
